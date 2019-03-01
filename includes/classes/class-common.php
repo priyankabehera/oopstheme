40f8a6cac08172c5f528f6daba74c597d2ffdecf
priyankabehera <priyanka.behera@multidots.com>
2019-03-01 15:38:35 +0530

    added the main class file

---
+++ includes/classes/class-common.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * File contains the main class.
+ *
+ * @since 1.0
+ */
+
+/**
+ * Class OopsTheme this is the main class having all the common classes.
+ *
+ */
+class OopsTheme {
+
+}
